ac7793abba49b94aab40e0971e2a2a809ece03c2
DTH235673-Khang <khang_dth235673@student.agu.edu.vn>
2026-02-05 16:28:22 +0700

    Add BuoiTH3

--- QuanLyBanHang/Forms/frmSanPham.cs
+++ QuanLyBanHang/Forms/frmSanPham.cs
@@ -1,17 +1,5 @@
 ﻿using QuanLyBanHang.Data;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.RegularExpressions;
-using System.Diagnostics;
 
 namespace QuanLyBanHang.Forms
 {
@@ -235,9 +223,15 @@ namespace QuanLyBanHang.Forms
             {
                 string fileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
                 string ext = Path.GetExtension(openFileDialog.FileName);
-                string fileSavePath = Path.Combine(imagesFolder, fileName.GenerateSlug() + ext);
-                File.Copy(openFileDialog.FileName, fileSavePath, true);
-                id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+
+                string newImage = fileName.GenerateSlug() + ext;
+                string fileSavePath = Path.Combine(imagesFolder, newImage);
+                using(var stream=new FileStream(fileSavePath, FileMode.Open, FileAccess.Read))
+                {
+                    picHinhAnh.Image=Image.FromStream(stream);
+                }
+                
+                id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value);
                 SanPham sp = context.SanPham.Find(id);
                 sp.HinhAnh = fileName.GenerateSlug() + ext;
                 context.SanPham.Update(sp);
